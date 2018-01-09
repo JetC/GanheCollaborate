21cb48bc849e1191dcee6754cffd3ef54c5620af
Sun PeiFeng <g7733pf@gmail.com>
2018-01-09 18:49:28 +0800

    小修改，但是有偏移。。

--- search.js
+++ search.js
@@ -1049,19 +1049,20 @@ loadJSON(function (response) {
                 }
             });
         });
-        $('#roam').click(function () {
+        $('#pointRoam').click(function () {
             roam();
         });
 
         viewer.canvas.addEventListener('click', function(e){
             var mousePosition = new Cesium.Cartesian2(e.clientX, e.clientY);
-            var ellipsoid = viewer.scene.globe.ellipsoid;
+            var ellipsoid = Cesium.Ellipsoid.WGS84;
             var cartesian = viewer.camera.pickEllipsoid(mousePosition, ellipsoid);
             if (cartesian) {
                 var cartographic = ellipsoid.cartesianToCartographic(cartesian);
                 var longitudeString = Cesium.Math.toDegrees(cartographic.longitude);
                 var latitudeString = Cesium.Math.toDegrees(cartographic.latitude);
                 var point = Cesium.Cartesian3.fromDegrees(longitudeString,latitudeString,150);
+
                 points.push(point);
 
                 console.log(longitudeString + ', ' + latitudeString);
@@ -1119,11 +1120,48 @@ loadJSON(function (response) {
         });
     }
     function roam() {
-        if (indexOfPointsToFlyTo<points.length-1) {
+        if (indexOfPointsToFlyTo<points.length) {
             fly(points[indexOfPointsToFlyTo]);
             indexOfPointsToFlyTo++;
+        } else if (indexOfPointsToFlyTo===points.length){
+            indexOfPointsToFlyTo = 0;
+            points = [];
         }
     }
+// //定义一些常量
+// var x_PI = 3.14159265358979324 * 3000.0 / 180.0;
+// var PI = 3.1415926535897932384626;
+// var a = 6378245.0;
+// var ee = 0.00669342162296594323;
+// function transformlat(lng, lat) {
+//     var ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + 0.1 * lng * lat + 0.2 * Math.sqrt(Math.abs(lng));
+//     ret += (20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) * 2.0 / 3.0;
+//     ret += (20.0 * Math.sin(lat * PI) + 40.0 * Math.sin(lat / 3.0 * PI)) * 2.0 / 3.0;
+//     ret += (160.0 * Math.sin(lat / 12.0 * PI) + 320 * Math.sin(lat * PI / 30.0)) * 2.0 / 3.0;
+//     return ret
+// }
+//
+// function transformlng(lng, lat) {
+//     var ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + 0.1 * lng * lat + 0.1 * Math.sqrt(Math.abs(lng));
+//     ret += (20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) * 2.0 / 3.0;
+//     ret += (20.0 * Math.sin(lng * PI) + 40.0 * Math.sin(lng / 3.0 * PI)) * 2.0 / 3.0;
+//     ret += (150.0 * Math.sin(lng / 12.0 * PI) + 300.0 * Math.sin(lng / 30.0 * PI)) * 2.0 / 3.0;
+//     return ret
+// }
+// function gcj02towgs84(lng, lat) {
+//
+//         var dlat = transformlat(lng - 105.0, lat - 35.0);
+//         var dlng = transformlng(lng - 105.0, lat - 35.0);
+//         var radlat = lat / 180.0 * PI;
+//         var magic = Math.sin(radlat);
+//         magic = 1 - ee * magic * magic;
+//         var sqrtmagic = Math.sqrt(magic);
+//         dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * PI);
+//         dlng = (dlng * 180.0) / (a / sqrtmagic * Math.cos(radlat) * PI);
+//         mglat = lat + dlat;
+//         mglng = lng + dlng;
+//         return [lng * 2 - mglng, lat * 2 - mglat]
+// }
 
 
 
